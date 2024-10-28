f31adc6d45a9fb3fd37b40f1521ab5148dc10fed
fpachilla <fpachlaboral@gmail.com>
2024-10-28 20:50:00 -0300

    Se cambia la config del puerto en el server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const sql = require('mssql');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configuración de la conexión
 const config = {
