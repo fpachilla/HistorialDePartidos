bfb3c7ba0d164424265b79cb5387a200587813fb
fpachilla <fpachlaboral@gmail.com>
2024-11-02 15:45:40 -0300

    Se añade log del puerto del server

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const sql = require('mssql');
 
 const app = express();
-const port = 4200;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 // Configuración de la conexión
 const config = {
