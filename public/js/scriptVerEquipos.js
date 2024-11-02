c418be65e027d4dc2cf0ab232b768e198a1a79d5
fpachilla <fpachlaboral@gmail.com>
2024-11-02 00:26:09 -0300

    Cambio orsa

--- public/js/scriptVerEquipos.js
+++ public/js/scriptVerEquipos.js
@@ -3,9 +3,9 @@
 document.addEventListener('DOMContentLoaded', async function() {
     try {
         const response = await fetch('/api/getUsuarios');
-        if (!response.ok) {
+        /*if (!response.ok) {
             throw new Error('Error al obtener los usuarios al hacer fetch');
-        }
+        }*/
         const usuarios = await response.json();
         
         const selectUser = document.getElementById('seleccionUsuario');
